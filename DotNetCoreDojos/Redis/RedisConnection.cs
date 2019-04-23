ccc83b5fdcdbb3de5b289bac8b268123ba2e292f
Stefan Weinschuetz <stefan.weinschuetz@ksb.com>
2019-04-23 07:16:09 +0200

    Add another redis test

--- DotNetCoreDojos/Redis/RedisConnection.cs
+++ DotNetCoreDojos/Redis/RedisConnection.cs
@@ -51,31 +51,6 @@ namespace Redis
             string testkey = DateTime.Now.ToShortDateString() + "_" + DateTime.Now.ToLongTimeString();
             string testvalue = DateTime.Now.ToLongTimeString();
 
-            db.StringSet(testkey, testvalue, new TimeSpan(0,0,5));
-
-            Thread.Sleep(4000);
-
-            var resultBeforeLifetimeIsExpired = db.StringGet(testkey);
-
-            Thread.Sleep(2000);
-
-            var resultAfterLifetimeIsExpired = db.StringGet(testkey);
-        }
-
-        /// <summary>
-        /// The test only works if a redis is available. This can be started with a docker run command:
-        /// docker run --name dojoredis -p 6379:6379 -d redis
-        /// </summary>
-        [Test]
-        public void RedisStressTestWithLifeTimeOf60Seconds()
-        {
-            ConnectionMultiplexer multiplexer = ConnectionMultiplexer.Connect("localhost:6379");
-
-            IDatabase db = multiplexer.GetDatabase();
-
-            string testkey = DateTime.Now.ToShortDateString() + "_" + DateTime.Now.ToLongTimeString();
-            string testvalue = DateTime.Now.ToLongTimeString();
-
             db.StringSet(testkey, testvalue, new TimeSpan(0, 0, 5));
 
             Thread.Sleep(4000);
@@ -88,7 +63,9 @@ namespace Redis
             // Here The value is not available, because the lifetime is 5s and in sum 6s are elapsed.
             var resultAfterLifetimeIsExpired = db.StringGet(testkey);
 
-            Assert.Fail();
+            Assert.IsTrue(resultBeforeLifetimeIsExpired.HasValue);
+
+            Assert.IsFalse(resultAfterLifetimeIsExpired.HasValue);
         }
 
         /// <summary>
@@ -96,6 +73,8 @@ namespace Redis
         /// docker run --name dojoredis -p 6379:6379 -d redis
         /// 
         /// Start a portainer container and watch the statistics (memory, CPU-usage and NetWork)
+        /// You should see that the memory an cache value increase to approx. 750MB.
+        /// After 60 seconds the values decrease, because redis clease the expired data.
         /// </summary>
         [Test]
         public void TestWithALotOfData()
@@ -108,10 +87,19 @@ namespace Redis
             string testvalue = DateTime.Now.ToLongTimeString();
 
 
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 50000; i++)
             {
                 // Store a big string
                 db.StringSet(i.ToString(), @"suhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslf
+                                            suhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslf
+                                            suhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslf
+                                            suhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslf
+                                            suhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslf
+                                            suhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslf
+                                            suhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslf
+                                            suhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslf
+                                            suhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslf
+                                            suhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslf
                                             suhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslf
                                             suhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslf
                                             suhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslfsuhdfkjshndfkjsafdsjfdsndfsnfsjdflsjfdljslf
@@ -124,9 +112,8 @@ namespace Redis
 
             }
 
-            Assert.Fail();
+            //On my machine it took 50s to get here.
+            int testbreakpoint = 1337;
         }
     }
-
-
 }
