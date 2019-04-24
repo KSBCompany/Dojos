81205fb8c36c6205379f74db9b767154075e00f4
Stefan Weinschuetz <stefan.weinschuetz@ksb.com>
2019-04-24 13:39:09 +0200

    Add some plain classes

---
+++ DotNetCoreDojos/Filesystem/LoadFromFile.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+namespace FileSystem
+{
+    public class LoadFromFile
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void LoadTextFromFile()
+        {
+            Assert.Fail();
+        }
+    }
+
+
+}
---
+++ DotNetCoreDojos/xsd-validation/xsd-validation.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+
+namespace Xsd
+{
+    public class XsdValidation
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Fail();
+        }
+    }
+
+
+}
