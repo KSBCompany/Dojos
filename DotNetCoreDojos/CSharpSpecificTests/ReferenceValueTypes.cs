8d779646e173f443665932c7afdbd9133512a54e
StefanWeinschuetz <stefan.weinschuetz@ksb.com>
2019-05-15 13:07:56 +0200

    add example

---
+++ DotNetCoreDojos/CSharpSpecificTests/ReferenceValueTypes.cs
@@ -0,0 +1,129 @@
+﻿using NUnit.Framework;
+using System.Collections.Concurrent;
+using System.Linq;
+
+namespace CSharpSpecificTests
+{
+    public class ReferenceValueTypes
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        /// Classes are reference types (like pointers) to objects on the heap.
+        [Test]
+        public void ExampleReferenceType()
+        {
+            Student a = new Student() { Age = 1, Name = "StudentA" };
+
+            Student b = a;
+
+            Assert.AreEqual("StudentA", b.Name);
+
+            b.Name = "StudentB";
+
+            Assert.AreEqual("StudentB", a.Name);
+
+            Change(a);
+
+            Assert.AreEqual("Superman", b.Name);
+        }
+
+        /// Structs are value types.
+        [Test]
+        public void ExampleValueType()
+        {
+            StudentStruct a = new StudentStruct() { Age = 1, Name = "StudentA" };
+
+            StudentStruct b = a;
+
+            Assert.AreEqual("StudentA", b.Name);
+
+            b.Name = "StudentB";
+
+            //See the difference to the class example!
+            Assert.AreEqual("StudentA", a.Name);
+
+            //See the difference to the class example!
+            Change(a);
+
+            //See the difference to the class example!
+            Assert.AreEqual("StudentA", a.Name);
+        }
+
+        [Test]
+        public void ExampleValueType_integer()
+        {
+            int i = 1;
+
+            Change(i);
+
+            Assert.AreEqual(1, i);
+
+            Change(ref i);
+
+            Assert.AreEqual(2, i);
+        }
+
+
+        /// <summary>
+        /// String is a reference type (string are in heap and immutable)
+        /// In heap because of size (stack is limited)
+        /// Immutable behause of multithreading.
+        /// </summary>
+        [Test]
+        public void Example_string()
+        {
+            string orig = "orig";
+
+            Change(orig);
+
+            Assert.AreEqual("orig", orig);
+        }
+
+        public void Change(Student s)
+        {
+            s.Age = 100;
+            s.Name = "Superman";
+        }
+
+        public void Change(StudentStruct s)
+        {
+            s.Age = 100;
+            s.Name = "Superman";
+        }
+
+        public void Change(int i)
+        {
+            i = 2;
+        }
+
+        public void Change(ref int i)
+        {
+            i = 2;
+        }
+
+        public void Change(string str)
+        {
+            str = "changed";
+        }
+
+
+        public class Student
+        {
+            public string Name { get; set; }
+
+            public int Age { get; set; }
+        }
+
+        public struct StudentStruct
+        {
+            public string Name { get; set; }
+
+            public int Age { get; set; }
+        }
+    }
+
+
+}
--- DotNetCoreDojos/LINQ/Linq.cs
+++ DotNetCoreDojos/LINQ/Linq.cs
@@ -85,9 +85,83 @@ namespace DotNetCoreDojos.LINQ
                 currentListElement.intVal *= 10;
             }
 
-            Assert.AreEqual(10, completeList[0]);
-            Assert.AreEqual(2, completeList[1]);
-            Assert.AreEqual(30, completeList[1]);
+            Assert.AreEqual(10, completeList[0].intVal);
+            Assert.AreEqual(2, completeList[1].intVal);
+            Assert.AreEqual(30, completeList[2].intVal);
+        }
+
+
+        /// <summary>
+        /// Like the example before, but now the subset of the list must be updated by another list.
+        /// This only works when a class is selected. Please see the difference with the next test.
+        /// </summary>
+        [Test]
+        public void UpdateSpecificElementsFromAnotherList()
+        {
+            List<BoolIntClass> completeList = new List<BoolIntClass>()
+            {
+                new BoolIntClass(){boolVal = true, intVal = 1},
+                new BoolIntClass(){boolVal = false, intVal = 2},
+                new BoolIntClass(){boolVal = true, intVal = 3}
+            };
+
+            List<int> updatedValues = new List<int>()
+            {
+                1337,
+                42
+            };
+
+
+            var filtered = from current in completeList
+                               where current.boolVal
+                               select current;
+
+            var FilteredList = filtered.ToList();
+
+            for(int i = 0; i < FilteredList.Count; i++)
+            {
+                FilteredList[i].intVal = updatedValues[i];
+            }
+
+            Assert.AreEqual(1337, completeList[0].intVal);
+            Assert.AreEqual(2, completeList[1].intVal);
+            Assert.AreEqual(42, completeList[2].intVal);
+        }
+
+        /// <summary>
+        /// In this test we select the integers in the class. Then the original values are not updated!
+        /// </summary>
+        [Test]
+        public void UpdateSpecificElementsFromAnotherList_ValueType()
+        {
+            List<BoolIntClass> completeList = new List<BoolIntClass>()
+            {
+                new BoolIntClass(){boolVal = true, intVal = 1},
+                new BoolIntClass(){boolVal = false, intVal = 2},
+                new BoolIntClass(){boolVal = true, intVal = 3}
+            };
+
+            List<int> updatedValues = new List<int>()
+            {
+                1337,
+                42
+            };
+
+
+            var filtered = from current in completeList
+                           where current.boolVal
+                           select current.intVal;
+
+            var FilteredList = filtered.ToList();
+
+            for (int i = 0; i < FilteredList.Count; i++)
+            {
+                FilteredList[i] = updatedValues[i];
+            }
+
+            Assert.AreEqual(1, completeList[0].intVal);
+            Assert.AreEqual(2, completeList[1].intVal);
+            Assert.AreEqual(3, completeList[2].intVal);
         }
 
 
