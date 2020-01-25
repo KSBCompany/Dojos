9e99113235392fbbb83010eb37927ede3889080c
Stefan W <weinschuetz.stefan@googlemail.com>
2020-01-25 14:09:50 +0100

    Add first React-Bootstrap

--- ReactApp/stefan/src/index.js
+++ ReactApp/stefan/src/index.js
@@ -3,10 +3,44 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Alert from 'react-bootstrap/Alert';
+import Button from 'react-bootstrap/Button';
+import { useState } from 'react';
+
+//https://react-bootstrap.github.io/getting-started/introduction/
+
+
+function AlertDismissible() {
+    const [show, setShow] = useState(true);
+  
+    return (
+      <>
+        <Alert show={show} variant="success">
+          <Alert.Heading>How's it going?!</Alert.Heading>
+          <p>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget
+            lacinia odio sem nec elit. Cras mattis consectetur purus sit amet
+            fermentum.
+          </p>
+          <hr />
+          <div className="d-flex justify-content-end">
+            <Button onClick={() => setShow(false)} variant="outline-success">
+              Close me ya'll!
+            </Button>
+          </div>
+        </Alert>
+  
+        {!show && <Button onClick={() => setShow(true)}>Show Alert</Button>}
+      </>
+    );
+  }
+  
+  ReactDOM.render(<AlertDismissible />, document.getElementById('root'));
 
 
 
-var element = React.createElement('h1', {className: 'greeting'}, 'Hellooo, world!');
-ReactDOM.render(element, document.getElementById('root'));
-
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
