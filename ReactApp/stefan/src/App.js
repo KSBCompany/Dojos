46ae23dc17f521c96ed7e0ce7972372be9ec6dfd
Stefan W <weinschuetz.stefan@googlemail.com>
2020-01-31 17:47:17 +0100

    Add a "Home" and finished the router

--- ReactApp/stefan/src/App.js
+++ ReactApp/stefan/src/App.js
@@ -4,14 +4,18 @@ import Shop from './Shop';
 import About from './About';
 import MyNav from './Nav';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+import Home from './Home';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <MyNav/>
-        <Route path="/about" component={About} />
-        <Route path="/shop" component={Shop} />
+        <Switch>
+          <Route path="/" exact component={Home}/>
+          <Route path="/about" component={About} />
+          <Route path="/shop" component={Shop} />
+        </Switch>
       </div>
     </Router>
   );
---
+++ ReactApp/stefan/src/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function Home() {
+  return (
+    <div>
+      <h1>Welcome to my page :-)</h1>
+    </div>
+  );
+}
+
+export default Home;
