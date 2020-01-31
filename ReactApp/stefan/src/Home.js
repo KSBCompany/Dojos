8494dca9fcfe258680b7d9278f39111dbad32b3f
Stefan W <weinschuetz.stefan@googlemail.com>
2020-01-31 18:26:20 +0100

    Add a dark table

--- ReactApp/stefan/src/Home.js
+++ ReactApp/stefan/src/Home.js
@@ -1,10 +1,40 @@
 import React from 'react';
 import './App.css';
+import Table from 'react-bootstrap/Table'
 
 function Home() {
   return (
     <div>
-      <h1>Welcome to my page :-)</h1>
+        <Table striped bordered hover variant="dark">
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>First Name</th>
+              <th>Last Name</th>
+              <th>Username</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>1</td>
+              <td>Stefan</td>
+              <td>Weinschütz</td>
+              <td>King</td>
+            </tr>
+            <tr>
+              <td>2</td>
+              <td>Eve</td>
+              <td>Weinschütz</td>
+              <td>evelknievel</td>
+            </tr>
+            <tr>
+              <td>3</td>
+              <td>Cleo</td>
+              <td>Weinschütz</td>
+              <td>meckerkatze</td>
+            </tr>
+          </tbody>
+        </Table>
     </div>
   );
 }
