fc2a06f69e197097d68885c3b474067ddc2519da
Stefan W <weinschuetz.stefan@googlemail.com>
2020-01-31 17:59:43 +0100

    Add a image

--- ReactApp/stefan/src/Shop.js
+++ ReactApp/stefan/src/Shop.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Image from 'react-bootstrap/Image'
 
 function Shop() {
   return (
     <div>
       <h1>Shop</h1>
+      <Image src="Nice.png" fluid/>
     </div>
   );
 }
